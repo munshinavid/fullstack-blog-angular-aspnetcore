4609b3ab6571308624291c6bebdb0b99bef284e4
munshinavid <navidmunshi24@gmail.com>
2026-04-02 01:48:14 +0600

    add environments setup

--- ui/CodePulse-UI/src/app/features/category/services/category-services.ts
+++ ui/CodePulse-UI/src/app/features/category/services/category-services.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, httpResource } from '@angular/common/http';
 import {AddCategoryRequest, Category } from "../models/category.model";  
+import { environment } from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CategoryServices {
-  private apiUrl = 'https://localhost:7071'; // Replace with your actual API URL
+  private apiUrl = environment.apiUrl; // Replace with your actual API URL
 
   constructor(private http: HttpClient) {}
 
